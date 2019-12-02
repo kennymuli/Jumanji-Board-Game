ed5466d094d4c70c7320d4d29bc8228e6d435a3c
Kenny <kenny@worthyt.io>
2019-12-02 16:17:17 -0500

    Update

--- game.py
+++ game.py
@@ -1,9 +1,43 @@
 from random import randint as rand
 from time import sleep
+import board
+import busio
+import adafruit_pca9685 as adafruit
+i2c = busio.I2C(board.SCL, board.SDA)
+hat = adafruit.PCA9685(i2c)
 
-#VARIABLES
-player1 = {'score':0,'skip':False}
-player2 = {'score':0,'skip': False}
+#Servo Motors
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
+#PI Board Variables
+hat.frequency = 60 #60hz
+#LED Lights speaks to hat channels (hat.channels[x])
+
+#kit.servo[x].angle = 0 - 180, rotates the motor
+
+
+#Game Variables
+player1 = {
+	'score':0,
+	'skip':False
+	'motor':0
+	}
+player2 = {
+	'score':0,
+	'skip':False
+	'motor':1
+	}
+player3 = {
+	'score':0,
+	'skip':False
+	'motor':2
+	}
+player4 = {
+	'score':0,
+	'skip':False
+	'motor':3
+	}
 players = [player1,player2]
 dicemax = 3 #maximum dice roll
 steps = 7 #number of board positions
@@ -24,23 +58,29 @@ def welcome(): #players the welcome message
 
 #STEPS
 def step1():
+	print(chr(27) + "[2J")
 	print "You stumble upon a hostile wolfpack while beginning your journey. Prove that you are one of them by howling as loud as you can."
 
 def step2():
+	print(chr(27) + "[2J")
 	print "You need to cross a river to continue your journey. Hold your breath for 10 seconds and pretend like you're swimming!"
 
 def step3():
+	print(chr(27) + "[2J")
 	print "Monkeys are coming to try to steal your food! Slam the table really hard to scare them away."
 
 def step5():
+	print(chr(27) + "[2J")
 	backwards = 6 - rand(1,5) #number of steps you need to take backwards
 	print "You were chased by a buffalo. Move back", backwards, "steps."
 	return backwards
 
 def step6(): 
+	print(chr(27) + "[2J")
 	print "You wandered into the wrong part of the forest and were hunted down by an indigenous tribe, who scalped your head and left your bare-skulled body for the nearby anacondas. You die slowly. The other player wins."
 
 def step7():
+	print(chr(27) + "[2J")
 	print "CONFETTI!"
 
 def start_game(dicemax,steps,players):
--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import adafruit_pca9685 as adafruit
 i2c = busio.I2C(board.SCL, board.SDA)
 hat = adafruit.PCA9685(i2c)
 
+#Servo Motors
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
 #Testing the Servo Hat to make sure that the Raspberry Pi accurately detects it.
 
 hat.frequency = 60 #60hz
