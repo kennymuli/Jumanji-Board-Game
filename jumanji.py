0fdc982cccfddb4f5c8cab5c482fe20b91b09af7
Kenny <kenny@worthyt.io>
2019-12-02 23:49:46 -0500

    Update jumanji.py

--- jumanji.py
+++ jumanji.py
@@ -171,24 +171,34 @@ def welcome(): #players the welcome message
 def step1():
 	print(chr(27) + "[2J")
 	print ("You stumble upon a hostile wolfpack while beginning your journey. Prove that you are one of them by howling as loud as you can.")
+	input()
+	print(chr(27) + "[2J")
 
 def step2():
 	print(chr(27) + "[2J")
 	print ("You need to cross a river to continue your journey. Hold your breath for 10 seconds and pretend like you're swimming!")
+	input()
+	print(chr(27) + "[2J")
 
 def step3():
 	print(chr(27) + "[2J")
 	print ("Monkeys are coming to try to steal your food! Slam the table really hard to scare them away.")
+	input()
+	print(chr(27) + "[2J")
 
 def step5():
 	print(chr(27) + "[2J")
 	backwards = 6 - rand(1,5) #number of steps you need to take backwards
 	print ("You were chased by a buffalo. Move back", backwards, "steps.")
 	return backwards
+	input()
+	print(chr(27) + "[2J")
 
 def step6(): 
 	print(chr(27) + "[2J")
 	print ("You wandered into the wrong part of the forest and were hunted down by an indigenous tribe, who scalped your head and left your bare-skulled body for the nearby anacondas. You die slowly. The other player wins.")
+	input()
+	print(chr(27) + "[2J")
 
 def start_game(dicemax,steps,players):
 	turn = 0
