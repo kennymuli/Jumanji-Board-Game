132b9d500d948fcc8428bd14460d58d90f8cfbfd
Kenny <kenny@worthyt.io>
2019-12-09 20:31:50 -0500

    5 steps for the board

--- game.py
+++ game.py
@@ -33,8 +33,8 @@ player1 = {
 	}
 
 players = [player1]
-dicemax = 3 #maximum dice roll
-steps = 7 #number of board positions
+dicemax = 2 #maximum dice roll
+steps = 5 #number of board positions
 
 #Diceroll Function
 def diceroll(dicemax): #roll the dice
@@ -158,29 +158,17 @@ def step1():
 	input()
 	print(chr(27) + "[2J")
 
-def step2():
-	print(chr(27) + "[2J")
-	print ("You need to cross a river to continue your journey. Hold your breath for 10 seconds and pretend like you're swimming!")
-	input()
-	print(chr(27) + "[2J")
-
 def step3():
 	print(chr(27) + "[2J")
 	print ("Monkeys are coming to try to steal your food! Slam the table really hard to scare them away.")
 	input()
 	print(chr(27) + "[2J")
 
-def step5():
+def backwards():
 	print(chr(27) + "[2J")
 	backwards = 6 - rand(1,5) #number of steps you need to take backwards
 	print ("You were chased by a buffalo. Move back", backwards, "steps.")
 	return backwards
-	input()
-	print(chr(27) + "[2J")
-
-def step6(): 
-	print(chr(27) + "[2J")
-	print ("You wandered into the wrong part of the forest and were hunted down by an indigenous tribe, who scalped your head and left your bare-skulled body for the nearby anacondas. You die slowly. The other player wins.")
 	input()
 	print(chr(27) + "[2J")
 
@@ -205,26 +193,23 @@ def start_game(dicemax,steps,players,player1,kit):
 
 		##########
 		#TRIGGERS#
-		##########
-		if players[turn]['score'] == 6: #dead
-			step6()
-			break
-		if players[turn]['score'] == 5: #move backwards 
-			backwardsteps = step5()
+		##########		
+		if players[turn]['score'] == 4: #wins
+			backwardsteps = backwards()
 			players[turn]['score'] = players[turn]['score'] - backwardsteps
 			kit.servo[0].angle = 84 #start the engine to move the piece
 			sleep(player1['enginetime']*backwardsteps)
 			kit.servo[0].angle = 86 #stop moving
-		
-		if players[turn]['score'] == 4: #skip a turn
-			players[turn]['skip'] = True
-			print ("Sorry, you're stuck in quicksand!")
 
 		if players[turn]['score'] == 1:
 			step1()
 
 		if players[turn]['score'] == 2:
-			step2()
+			backwardsteps = backwards()
+			players[turn]['score'] = players[turn]['score'] - backwardsteps
+			kit.servo[0].angle = 84 #start the engine to move the piece
+			sleep(player1['enginetime']*backwardsteps)
+			kit.servo[0].angle = 86 #stop moving
 
 		if players[turn]['score'] == 3:
 			step3()
